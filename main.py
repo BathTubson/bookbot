c32bfd75d47e5f7d15c8cd55b519eccd6cdf2c4f
BathTubson <Bathompson0524@gmail.com>
2025-02-17 20:09:44 -0700

    A full analysis of Frankenstein

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ def main():
         print(file_contents)
         for word in file_contents.split():
             word_counter += 1
-        print(word_counter)
         return word_counter
+def letter_counter():
+    letter_dict = {}
+    with open("./books/book.txt") as f:
+        file_contents = f.read()
+        for letter in str(file_contents).lower():
+            if letter not in letter_dict:
+                letter_dict[letter] = 1
+            else:
+                letter_dict[letter] += 1
+    return letter_dict
 
+def letter_printer():
+    letter_dict = letter_counter()
+    for letter in letter_dict:
+        if letter.isalpha() == True:
+            print("The '" + letter + "' character was found " + str(letter_dict[letter]) + " times")
 
-
-
-
-
-
-
-
-
-
-main()
+letter_printer()
+#main()
