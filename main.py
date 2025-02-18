5567265c1d30f4c8d44b2cc74d196f9fd78e8893
BathTubson <Bathompson0524@gmail.com>
2025-02-17 19:42:03 -0700

    pastes and counts the words of Frankenstien

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    with open("./books/book.txt") as f:
+        word_counter = 0
+        file_contents = f.read()
+        print(file_contents)
+        for word in file_contents.split():
+            word_counter += 1
+        return word_counter
+
+
+
+
+
+
+
+
+
+
+
+main()
